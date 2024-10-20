721763cacefac4b9e07367d2090cd3c1d0bfb78b
hoihmy <myhoih@kms-technology.com>
2024-10-20 11:44:51 +0700

    Add custom metrics

---
+++ src/main/java/com/bezkoder/spring/data/mongodb/config/AppConfig.java
@@ -0,0 +1,52 @@
+package com.bezkoder.spring.data.mongodb.config;
+
+import io.micrometer.cloudwatch2.CloudWatchConfig;
+import io.micrometer.cloudwatch2.CloudWatchMeterRegistry;
+import io.micrometer.core.instrument.Clock;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient;
+
+import java.time.Duration;
+import java.util.Map;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public CloudWatchAsyncClient cloudWatchAsyncClient() {
+        return CloudWatchAsyncClient
+                .builder()
+                .region(Region.US_EAST_2)
+                .credentialsProvider(ProfileCredentialsProvider.create("hoihmyit"))
+                .build();
+    }
+
+    @Bean
+    public MeterRegistry getMeterRegistry() {
+        CloudWatchConfig cloudWatchConfig = setupCloudWatchConfig();
+
+        MeterRegistry meterRegistry = new CloudWatchMeterRegistry(cloudWatchConfig, Clock.SYSTEM,
+                cloudWatchAsyncClient());
+
+        return meterRegistry;
+    }
+
+    private CloudWatchConfig setupCloudWatchConfig() {
+        CloudWatchConfig cloudWatchConfig = new CloudWatchConfig() {
+
+            private Map<String, String> configuration
+                    = Map.of("cloudwatch.namespace", "tutorialsApp",
+                    "cloudwatch.step", Duration.ofMinutes(1).toString());
+
+            @Override
+            public String get(String key) {
+                return configuration.get(key);
+            }
+        };
+        return cloudWatchConfig;
+    }
+}
--- src/main/java/com/bezkoder/spring/data/mongodb/controller/TutorialController.java
+++ src/main/java/com/bezkoder/spring/data/mongodb/controller/TutorialController.java
@@ -1,13 +1,11 @@
 package com.bezkoder.spring.data.mongodb.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
+import com.bezkoder.spring.data.mongodb.model.Tutorial;
+import com.bezkoder.spring.data.mongodb.repository.TutorialRepository;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,8 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bezkoder.spring.data.mongodb.model.Tutorial;
-import com.bezkoder.spring.data.mongodb.repository.TutorialRepository;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 //@CrossOrigin(origins = "http://localhost:8081")
 //@CrossOrigin(origins = "*") // Allow all origins
@@ -27,97 +27,134 @@ import com.bezkoder.spring.data.mongodb.repository.TutorialRepository;
 @RequestMapping("/api")
 public class TutorialController {
 
-  @Autowired
-  TutorialRepository tutorialRepository;
+    @Autowired
+    TutorialRepository tutorialRepository;
 
-  @GetMapping("/tutorials")
-  public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title) {
-    try {
-      List<Tutorial> tutorials = new ArrayList<Tutorial>();
+    @Autowired
+    private MeterRegistry meterRegistry;
 
-      if (title == null)
-        tutorialRepository.findAll().forEach(tutorials::add);
-      else
-        tutorialRepository.findByTitleContaining(title).forEach(tutorials::add);
+    @GetMapping("/tutorials")
+    public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title) {
+        final long startTime = System.currentTimeMillis();
+        try {
+            List<Tutorial> tutorials = new ArrayList<Tutorial>();
 
-      if (tutorials.isEmpty()) {
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-      }
+            if (title == null)
+                tutorials.addAll(tutorialRepository.findAll());
+            else
+                tutorials.addAll(tutorialRepository.findByTitleContaining(title));
 
-      return new ResponseEntity<>(tutorials, HttpStatus.OK);
-    } catch (Exception e) {
-      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            meterRegistry.counter("tutorials.list").increment();
+
+            if (tutorials.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(tutorials, HttpStatus.OK);
+        } catch (Exception e) {
+            meterRegistry.counter("tutorials.error").increment();
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        } finally {
+            final long elapsedTime = System.currentTimeMillis() - startTime;
+            System.out.println("Elapsed time for fetching all tutorials data: " + elapsedTime);
+            meterRegistry.timer("execution.time.tutorials.list")
+                    .record(Duration.ofMillis(elapsedTime));
+        }
     }
-  }
 
-  @GetMapping("/tutorials/{id}")
-  public ResponseEntity<Tutorial> getTutorialById(@PathVariable("id") String id) {
-    Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
+    @GetMapping("/tutorials/{id}")
+    public ResponseEntity<Tutorial> getTutorialById(@PathVariable("id") String id) {
+        Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
 
-    if (tutorialData.isPresent()) {
-      return new ResponseEntity<>(tutorialData.get(), HttpStatus.OK);
-    } else {
-      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        if (tutorialData.isPresent()) {
+            return new ResponseEntity<>(tutorialData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
-  }
 
-  @PostMapping("/tutorials")
-  public ResponseEntity<Tutorial> createTutorial(@RequestBody Tutorial tutorial) {
-    try {
-      Tutorial _tutorial = tutorialRepository.save(new Tutorial(tutorial.getTitle(), tutorial.getDescription(), false));
-      return new ResponseEntity<>(_tutorial, HttpStatus.CREATED);
-    } catch (Exception e) {
-      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    @PostMapping("/tutorials")
+    public ResponseEntity<Tutorial> createTutorial(@RequestBody Tutorial tutorial) {
+        final long startTime = System.currentTimeMillis();
+        try {
+            Tutorial _tutorial = tutorialRepository.save(new Tutorial(tutorial.getTitle(), tutorial.getDescription(), false));
+            meterRegistry.counter("tutorials.create").increment();
+            return new ResponseEntity<>(_tutorial, HttpStatus.CREATED);
+        } catch (Exception e) {
+            meterRegistry.counter("tutorials.error").increment();
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        } finally {
+            final long elapsedTime = System.currentTimeMillis() - startTime;
+            System.out.println("Elapsed time for creating a tutorials data: " + elapsedTime);
+            meterRegistry.timer("execution.time.tutorials.create")
+                    .record(Duration.ofMillis(elapsedTime));
+        }
     }
-  }
 
-  @PutMapping("/tutorials/{id}")
-  public ResponseEntity<Tutorial> updateTutorial(@PathVariable("id") String id, @RequestBody Tutorial tutorial) {
-    Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
+    @PutMapping("/tutorials/{id}")
+    public ResponseEntity<Tutorial> updateTutorial(@PathVariable("id") String id, @RequestBody Tutorial tutorial) {
+        Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
 
-    if (tutorialData.isPresent()) {
-      Tutorial _tutorial = tutorialData.get();
-      _tutorial.setTitle(tutorial.getTitle());
-      _tutorial.setDescription(tutorial.getDescription());
-      _tutorial.setPublished(tutorial.isPublished());
-      return new ResponseEntity<>(tutorialRepository.save(_tutorial), HttpStatus.OK);
-    } else {
-      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        if (tutorialData.isPresent()) {
+            Tutorial _tutorial = tutorialData.get();
+            _tutorial.setTitle(tutorial.getTitle());
+            _tutorial.setDescription(tutorial.getDescription());
+            _tutorial.setPublished(tutorial.isPublished());
+            return new ResponseEntity<>(tutorialRepository.save(_tutorial), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
-  }
 
-  @DeleteMapping("/tutorials/{id}")
-  public ResponseEntity<HttpStatus> deleteTutorial(@PathVariable("id") String id) {
-    try {
-      tutorialRepository.deleteById(id);
-      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    } catch (Exception e) {
-      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    @DeleteMapping("/tutorials/{id}")
+    public ResponseEntity<HttpStatus> deleteTutorial(@PathVariable("id") String id) {
+        try {
+            tutorialRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
-  }
 
-  @DeleteMapping("/tutorials")
-  public ResponseEntity<HttpStatus> deleteAllTutorials() {
-    try {
-      tutorialRepository.deleteAll();
-      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    } catch (Exception e) {
-      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    @DeleteMapping("/tutorials")
+    public ResponseEntity<HttpStatus> deleteAllTutorials() {
+        final long startTime = System.currentTimeMillis();
+        try {
+            tutorialRepository.deleteAll();
+            meterRegistry.counter("tutorials.deleteAll").increment();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            meterRegistry.counter("tutorials.error").increment();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        } finally {
+            final long elapsedTime = System.currentTimeMillis() - startTime;
+            System.out.println("Elapsed time for deleting all tutorials data: " + elapsedTime);
+            meterRegistry.timer("execution.time.tutorials.deleteAll")
+                    .record(Duration.ofMillis(elapsedTime));
+        }
     }
-  }
 
-  @GetMapping("/tutorials/published")
-  public ResponseEntity<List<Tutorial>> findByPublished() {
-    try {
-      List<Tutorial> tutorials = tutorialRepository.findByPublished(true);
+    @GetMapping("/tutorials/published")
+    public ResponseEntity<List<Tutorial>> findByPublished() {
+        final long startTime = System.currentTimeMillis();
+        try {
+            List<Tutorial> tutorials = tutorialRepository.findByPublished(true);
 
-      if (tutorials.isEmpty()) {
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-      }
-      return new ResponseEntity<>(tutorials, HttpStatus.OK);
-    } catch (Exception e) {
-      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            meterRegistry.counter("tutorials.publish").increment();
+
+            if (tutorials.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(tutorials, HttpStatus.OK);
+        } catch (Exception e) {
+            meterRegistry.counter("tutorials.error").increment();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        } finally {
+            final long elapsedTime = System.currentTimeMillis() - startTime;
+            System.out.println("Elapsed time for publishing a tutorials data: " + elapsedTime);
+            meterRegistry.timer("execution.time.tutorials.deleteAll")
+                    .record(Duration.ofMillis(elapsedTime));
+        }
     }
-  }
 
 }
